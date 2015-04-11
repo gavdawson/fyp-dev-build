1aae63dd0a4a57ad215fea8959208bbe1ac5906c
Gav Dawson <gavdawsonleeds@gmail.com>
2015-04-11 12:26:17 +0100

    Updated progress
    Added more views and refined styles.

---
+++ views/playersummary.php
@@ -0,0 +1,378 @@
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB" lang="en-GB">
+
+
+<head>
+
+<title>ABSP - Ratings in Ranking Order</title>
+
+<!-- <link rel="stylesheet" type="text/css" href="/fyp-dev-build/css/bootstrap.css" /> -->
+
+<link rel="stylesheet" type="text/css" href="/fyp-dev-build/css/norm.css" />
+
+<link rel="stylesheet" type="text/css" href="/fyp-dev-build/css/style.css" />
+
+<link rel="stylesheet" type="text/css" href="/fyp-dev-build/css/jquery.dataTables.css" />
+
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="/fyp-dev-build/js/jquery.js"></script>
+<script src="/fyp-dev-build/js/table-sorter.js"></script>
+<script src="/fyp-dev-build/js/jquery.dataTables.min.js"></script>
+</head>
+
+
+<body>
+
+<div class="top"> 
+ 
+<img class="tops" src="/fyp-dev-build/images/absp_logo.PNG" width="204" height="80" alt="Association of British Scrabble Players" />
+ 
+</div>
+ 
+<div id="nav"> 
+ <ul>
+<li><a href="/index.shtml">Home</a> 
+</li>
+<li><a href="/absp/index.shtml">The ABSP</a> 
+<ul> 
+<li><a href="/absp/joinus.shtml">Join Us</a></li> 
+<li><a href="/absp/sponsors.shtml">Sponsors</a></li> 
+<li><a href="/absp/committee.shtml">Committee</a></li> 
+<li><a href="/absp/publications.shtml">Publications</a></li>
+
+<li><a href="/absp/qualifying.shtml">Qualifying</a></li><li><a href="/absp/credits.shtml">Credits</a></li>
+<li><a href="/absp/contactus.shtml">Contact Us</a></li>
+</ul> 
+</li> 
+<li><a href="/news/index.shtml">News</a> 
+<ul> 
+<li><a href="/news/archive.shtml">Archive</a></li> 
+<li><a href="/news/coverage.shtml">Coverage</a></li> 
+</ul> 
+</li> 
+<li><a href="/play/index.shtml">Play</a> 
+<ul>
+<li><a href="/play/newplayers.shtml">New Players</a></li> 
+<li><a href="/calendar/calendar.shtml">Calendar</a></li> 
+<li><a href="/play/clubs.shtml">Clubs</a></li>
+<li><a href="/play/experiences.shtml">Experiences</a></li>
+<li><a href="/play/playisc.shtml">ISC</a></li>
+<li><a href="/play/jumbletime.shtml">JumbleTime</a></li>
+<li><a href="/play/links.shtml">Links</a></li>
+<li><a href="/play/uk-scrabble.shtml">uk-scrabble</a></li>
+<li><a href="/play/youthscrabble.shtml">Youth Scrabble</a></li>
+<li><a href="/play/software.shtml">Software</a></li>
+</ul> 
+</li> 
+<li><a href="/results/index.shtml">Results</a> 
+<ul> 
+<li><a href="/results/ratings.shtml">Ratings</a>
+<ul>
+<li><a href="/results/JAGRatingsNotes.shtml">What are Ratings?</a></li>
+<li><a href="/results/ratings_detail.shtml">Rolling System</a></li>
+</ul>
+</li> 
+<li><a href="/results/majors.shtml">Majors</a></li> 
+<li><a href="/results/yearlylists.shtml">Yearly Lists</a>
+<ul>
+<li><a href="/results/results8790.shtml">1987-1990</a></li>
+<li>1991-1999<ul>
+<li><a href="/results/results1991.shtml">1991</a></li>
+<li><a href="/results/results1992.shtml">1992</a></li>
+<li><a href="/results/results1993.shtml">1993</a></li>
+<li><a href="/results/results1994.shtml">1994</a></li>
+<li><a href="/results/results1995.shtml">1995</a></li>
+<li><a href="/results/results1996.shtml">1996</a></li>
+<li><a href="/results/results1997.shtml">1997</a></li>
+<li><a href="/results/results1998.shtml">1998</a></li>
+<li><a href="/results/results1999.shtml">1999</a></li>
+</ul>
+</li>
+<li>2000-2009<ul>
+<li><a href="/results/results2000.shtml">2000</a></li>
+<li><a href="/results/results2001.shtml">2001</a></li>
+<li><a href="/results/results2002.shtml">2002</a></li>
+<li><a href="/results/results2003.shtml">2003</a></li>
+<li><a href="/results/results2004.shtml">2004</a></li>
+<li><a href="/results/results2005.shtml">2005</a></li>
+<li><a href="/results/results2006.shtml">2006</a></li>
+<li><a href="/results/results2007.shtml">2007</a></li>
+<li><a href="/results/results2008.shtml">2008</a></li>
+<li><a href="/results/results2009.shtml">2009</a></li>
+</ul>
+</li><li>2010-2013<ul>
+<li><a href="/results/results2010.shtml">2010</a></li>
+<li><a href="/results/results2011.shtml">2011</a></li>
+<li><a href="/results/results2012.shtml">2012</a></li>
+<li><a href="/results/results2013.shtml">2013</a></li></ul></li>
+</ul>
+</li>
+<li><a href="/results/moneylists.shtml">Money Lists</a>
+<ul>
+<li><a href="/results/Money%20List%202007.xls">2007 (.xls)</a></li>
+<li><a href="/results/Money%20List%202008.xls">2008 (.xls)</a></li>
+<li><a href="/results/Money%20List%202009.xls">2009 (.xls)</a></li>
+<li><a href="/results/Money%20List%202010.xls">2010 (.xls)</a></li>
+<li><a href="/results/moneylist2011.xls">2011 (.xls)</a></li>
+<li><a href="/results/moneylist2012.xls">2012 (.xls)</a></li>
+<li><a href="/results/moneylist2013.xls">2013 (.xls)</a></li>
+<li><a href="/results/moneylist2014jul.xls">2014 (.xls)</a></li>
+</ul>
+</li> 
+<li><a href="/results/records.shtml">Records</a></li>
+<li><a href="/results/titleawards.shtml">Titles &amp; Awards</a></li>
+</ul> 
+</li>
+<li><a href="/words/index.shtml">Words</a> 
+<ul>
+<li><a href="/words/atoz.shtml">A to Z</a></li>
+<li><a href="/words/culture.shtml">Culture</a>
+<ul>
+<li><a href="/words/costume.shtml">Costume</a></li>
+<li><a href="/words/dances.shtml">Dances</a></li>
+<li><a href="/words/drinks.shtml">Drinks</a></li>
+<li><a href="/words/food.shtml">Food</a></li>
+<li><a href="/words/music.shtml">Music</a></li>
+<li><a href="/words/myth.shtml">Myth</a></li>
+<li><a href="/words/names.shtml">Names</a></li>
+<li><a href="/words/poetry.shtml">Poetry</a></li>
+<li><a href="/words/religion.shtml">Religion</a></li>
+</ul>
+</li>
+<li><a href="/words/languages.shtml">Languages</a>
+<ul>
+<li><a href="/words/afrcze.shtml">Afr-Cze</a></li>
+<li><a href="/words/danhind.shtml">Dan-Hind</a></li>
+<li><a href="/words/hingkor.shtml">Hing-Kor</a></li>
+<li><a href="/words/latnaam.shtml">Lat-NaAm</a></li>
+<li><a href="/words/naauque.shtml">NaAu-Que</a></li>
+<li><a href="/words/romsin.shtml">Rom-Sin</a></li>
+<li><a href="/words/somton.shtml">Som-Ton</a></li>
+<li><a href="/words/tupzulu.shtml">Tup-Zulu</a></li>
+</ul>
+</li>
+<li><a href="/words/linguistics.shtml">Linguistics</a>
+<ul>
+<li><a href="/words/langscripts.shtml">Alphabets, Scripts etc.</a></li>
+<li><a href="/words/grammar.shtml">Grammar and Semantics</a></li>
+<li><a href="/words/interjections.shtml">Interjections</a></li>
+<li><a href="/words/irregverbs.shtml">Non-standard Verb Forms</a></li>
+<li><a href="/words/nsverbs.pdf">Non-standard Verb Forms (.PDF)</a></li>
+<li><a href="/words/langpronounce.shtml">Pronunciation</a></li>
+<li><a href="/words/langrhetoric.shtml">Rhetoric</a></li>
+<li><a href="/words/timehowoften.shtml">Time How Often</a></li>
+<li><a href="/words/timehowoften.doc">Time How Often (.doc)</a></li>
+<li><a href="/words/timewhen.shtml">Time When</a></li>
+<li><a href="/words/langvariants.shtml">Variants</a></li>
+</ul>
+</li>
+<li><a href="/words/miscellany.shtml">Miscellany</a>
+<ul>
+<li><a href="/words/acronyms.shtml">Acronyms</a></li>
+<li><a href="/words/bags.shtml">Bags</a></li>
+<li><a href="/words/baskets.shtml">Baskets</a></li>
+<li><a href="/words/boats.shtml">Boats</a></li>
+<li><a href="/words/boxes.shtml">Boxes &amp; Cases</a></li>
+<li><a href="/words/colours.shtml">Colours</a></li>
+<li><a href="/words/fursandskins.shtml">Furs &amp; Skins</a></li>
+<li><a href="/words/ropes.shtml">Ropes, Straps &amp; Thongs</a></li>
+<li><a href="/words/textiles.shtml">Textiles</a></li>
+</ul>
+</li>
+<li><a href="/words/sciencenature.shtml">Science &amp; Nature</a>
+<ul>
+<li><a href="/words/acids.shtml">Acids</a></li>
+<li><a href="/words/anatomy.shtml">Anatomy</a></li>
+<li><a href="/words/computing.shtml">Computing</a></li>
+<li><a href="/words/earth.shtml">Earth</a></li>
+<li><a href="/words/ine.shtml">-INE Words</a></li>
+<li><a href="/words/mammals.shtml">Mammals</a></li>
+<li><a href="/words/medical.shtml">Medical</a></li>
+<li><a href="/words/otherlife.shtml">Other Life</a></li>
+<li><a href="/words/physics.shtml">Physics</a></li>
+<li><a href="/words/plants.shtml">Plants</a></li>
+<li><a href="/words/timehowlong.shtml">Time How Long</a></li>
+<li><a href="/words/measurement.shtml">Units</a></li>
+</ul>
+</li>
+<li><a href="/words/society.shtml">Society</a>
+<ul>
+<li><a href="/words/heraldry.shtml">Heraldry</a></li>
+<li><a href="/words/law.shtml">Law</a></li>
+<li><a href="/words/britishmoney.shtml">Money, British</a></li>
+<li><a href="/words/foreignmoney.shtml">Money, Foreign</a></li>
+<li><a href="/words/rankstitles.shtml">Ranks &amp; Titles</a></li>
+<li><a href="/words/structures.shtml">Structures</a></li>
+</ul> 
+</li>
+<li><a href="/words/sportcombat.shtml">Sport &amp; Combat</a>
+<ul>
+<li><a href="/words/armour.shtml">Armour</a></li>
+<li><a href="/words/sport.shtml">Sport</a></li>
+<li><a href="/words/weapons.shtml">Weapons</a></li>
+</ul>
+</li>
+<li><a href="/words/study.shtml">Study</a>
+<ul>
+<li><a href="/words/2lw.shtml">2 Letter Words</a></li>
+<li><a href="/words/3lw.shtml">3 Letter Words</a></li>
+<li><a href="/words/astate.shtml">A- State Words</a></li>
+<li><a href="/words/nocomps.shtml">Adjectives no Comp'</a></li>
+<li><a href="/words/ful.shtml">-FUL and -LOAD</a></li>
+<li><a href="/words/noplurals.shtml">No -S Plural</a></li>
+<li><a href="/words/lynouns.shtml">Nouns +LY</a></li>
+<li><a href="/words/pitfalls.shtml">Pitfalls</a></li>
+<li><a href="/words/nosing.shtml">Plurals no Singular</a></li>
+<li><a href="/words/sic.shtml">Sic!</a></li>
+</ul>
+</li> 
+</ul> 
+</li>
+<li><a href="/photos/index.shtml">Photos</a> 
+<ul> 
+<li><a href="/photos/index.shtml">Tournaments</a></li> 
+<li>Players
+<ul>
+<li><a href="/photos/playersab.shtml">Surnames A-B</a></li>
+<li><a href="/photos/playerscf.shtml">Surnames C-F</a></li>
+<li><a href="/photos/playersgk.shtml">Surnames G-K</a></li>
+<li><a href="/photos/playerslo.shtml">Surnames L-O</a></li>
+<li><a href="/photos/playersps.shtml">Surnames P-S</a></li>
+<li><a href="/photos/playerstz.shtml">Surnames T-Z</a></li>
+</ul>
+</li> 
+ </ul>
+</div> 
+
+    <div class="description"> 
+        <p>Player summary.
+    </div>
+
+    <div class="gav">
+<?php
+$servername 	= "localhost";
+$username 		= "root";
+$password 		= "mysql";
+$dbname 		= "absporgu_membership";
+$orderby 		= '';
+$sort 			= '';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// Initial SQL Query
+// $sql = "SELECT * FROM playero";
+
+// SQL for all playero info plus the count of tournaments and possibily the total points (TRP to be confirmed if this is points)
+$sql = "SELECT * FROM playero, (SELECT DISTINCT playerid, count(tournid) AS tourncount, TRP AS trptotal FROM `rpointso` GROUP BY playerid) tmp WHERE playero.playerid = tmp.playerid";
+
+// SQL for total matches played (not joined or as variable):
+// "SELECT DISTINCT playerid, sum(counttournid) as sumcounttourndid FROM (SELECT * FROM (SELECT DISTINCT playerid, count(tournid) as counttournid FROM `ratedmatches` GROUP BY playerid) t1 UNION SELECT * FROM (SELECT DISTINCT oppoid, count(tournid) as counttournid FROM `ratedmatches` GROUP BY oppoid) t2) t3 GROUP BY playerid"
+
+// $total_matches_played 			= "SELECT DISTINCT playerid, sum(counttournid) as sumcounttourndid FROM (SELECT * FROM (SELECT DISTINCT playerid, count(tournid) as counttournid FROM `ratedmatches` GROUP BY playerid) t1 UNION SELECT * FROM (SELECT DISTINCT oppoid, count(tournid) as counttournid FROM `ratedmatches` GROUP BY oppoid) t2) t3 GROUP BY playerid";
+// $total_tournaments_and_points 	= "SELECT DISTINCT playerid, count(tournid) as tourncount, TRP as trptotal FROM `rpointso` GROUP BY playerid";
+// $totals_join 					= "SELECT * FROM  (" . $total_matches_played . ") total_matches_played (" . $total_tournaments_and_points . ") total_tournaments_and_points,  WHERE total_matches_played.playerid = total_tournaments_and_points.playerid";
+// $sql_join 						= "SELECT * FROM playero, (". $totals_join .") totals_join WHERE playero.playerid = totals_join.playerid";
+
+// $sql 							= $sql_join;
+
+// Do the orderby
+// If the orderby querystring is set
+if( isset( $_GET['orderby'] ) ) {
+
+	// Init the sort order
+	$sort 		= 'ASC';
+	$orderby 	= $_GET['orderby'];
+
+	// if the query sort is set (which will always only be desc), set the order to DESC
+	if( isset( $_GET['sort'] ) && $_GET['sort'] == 'desc' ) {
+		$sort = 'DESC';
+	}
+
+	// Append to the SQL Query
+	$sql .= " ORDER BY " . $orderby . " " . $sort;
+}
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	?>
+    <table id="example" class="new-table row-border hover order-column" cellspacing="0" width="100%">        
+        <thead>
+            <tr>
+                
+            <th>
+                Name
+            </th>
+            <th>
+                Games
+            </th>
+    		<th>
+    			W
+    		</th>
+    		<th>
+    			L
+    		</th>
+    		<th>
+    			D
+    		</th>
+            <th>
+                W%
+            </th>
+            <th>
+                Tourneys
+            </th>
+            <th>
+                Peak Rating
+            </th>
+            <th>
+                Current Rating
+            </th>
+            <th>
+                Current Rank
+            </th>
+        </tr>
+    	</head>
+        <tbody  id="fbody">
+    <?php
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    	?>
+        
+        <tr>
+        	<td><?php echo $row["nameComposite"];?></td>
+            <td><?php echo $row["playerid"];?></td>
+            <td><?php echo $row["playerid"];?></td>
+        	<td><?php echo $row["playerid"];?></td>
+        	<td><?php echo $row["playerid"];?></td>        	
+            <td><?php echo $row["playerid"];?></td>
+            <td><?php echo $row["playerid"];?></td>
+            <td><?php echo $row["playerid"];?></td> 
+            <td><?php echo $row["playerid"];?></td> 
+            <td><?php echo $row["playerid"];?></td>                   
+        </tr>
+    
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
+</div>
+    <?php
+} else {
+	?>
+	<p>0 results</p>
+	<?php
+}
+$conn->close();
+?>
+
+</body>
